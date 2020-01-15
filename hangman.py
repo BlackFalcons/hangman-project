2f515a86b66a4a989ca2f00c690ef5eb80e5371f
Eivind Limstrand <brage211@gmail.com>
2020-01-15 15:15:09 +0100

    Add project files

---
+++ hangman.py
@@ -0,0 +1,33 @@
+from platform import system as currentplatform
+from os.path import isfile
+from requests import get
+from time import sleep
+from os import system as command
+from random import choice as get_random
+
+
+def downloadFile(url, filename):
+    file = get(url)
+    open(filename, "wb").write(file.content)
+
+
+def clearScreen():
+    if currentplatform() == "Windows":
+        command("cls")
+    else:
+        command("clear")
+
+
+if not isfile("ordliste.txt"):
+    print("File not found, downloading...")
+    fileLocation = "https://raw.githubusercontent.com/0301/ordliste/master/ordliste_snl_fellesord.txt"
+    downloadFile(fileLocation, "./ordliste.txt")
+    clearScreen()
+    print("Download complete!")
+    sleep(2)
+
+# Hangman kode starter her, dette er etter vi har lastet ned filen.
+if isfile("ordliste.txt"):
+    print("Welcome to hangman!\n")
+    hangman_word = open("./ordliste.txt", encoding="utf8").readlines()
+    print(f"Your random word is {get_random(hangman_word)}")
